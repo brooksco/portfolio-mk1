3b1302cc2db69417344de991f2f9d0a7c5ffe889
Colin Brooks <colinbooks@gmail.com>
2013-04-08 03:42:39 -0500

    production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,10 @@ Folio::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Possible Heroku fix
+  config.assets.initialize_on_precompile = false
+
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
